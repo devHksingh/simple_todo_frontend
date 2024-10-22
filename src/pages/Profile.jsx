ea13d648fd1d76398b7dedd040b9008ccfbe6f41
devHksingh <hksingh.dev@gmail.com>
2024-10-22 22:49:39 +0530

    add api call

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,49 @@
-// import React from 'react'
+
+
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 function Profile() {
+   
+    
+    const [data,setData] = useState({})
+    useEffect(()=>{
+        async function apiCall(){
+            const token = localStorage.getItem("token")
+            if(token){
+                try {
+                    const response = await axios.get('/api/users/userProfile',{
+                        headers:{
+                            'Authorization':`Bearer ${token}`
+                        }
+                    })
+                     if(response){
+                        console.log("apiData" ,response);
+                    //  setData(response.data)
+                    //  console.log(data);
+                     console.log("apiData",response);
+                     console.log("RES .DATA" ,response.data);
+                     console.log("RES .DATA PROFILE" ,response.data.profile);
+                     setData(response.data.profile)
+                     console.log("DATA #####" ,data);
+                     }
+                     
+                     
+                } catch (error) {
+                    console.log(error);
+                    
+                }
+            }
+            
+        }
+       
+        apiCall()
+        
+        
+        
+    }, [])
+    
+    
   return (
     <div>Profile</div>
   )
