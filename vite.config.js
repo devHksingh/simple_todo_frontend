2e86d37b09754cd820db45ee6b844f64adf0fc44
devHksingh <hksingh.dev@gmail.com>
2024-11-07 22:22:01 +0530

    change backend url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://simple-todo-backend-cntw.onrender.com',
+      '/api': 'https://simple-todo-backend-hfsz.onrender.com',
     },
   },
   plugins: [react()],
