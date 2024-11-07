76e076a129972a6aea383eafa18cf98b02c2a9eb
devHksingh <hksingh.dev@gmail.com>
2024-11-07 21:07:53 +0530

    test2

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      "/api": {
-        target: "https://simple-todo-backend-cntw.onrender.com",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
+      '/api': 'https://simple-todo-backend-cntw.onrender.com',
     },
   },
   plugins: [react()],
