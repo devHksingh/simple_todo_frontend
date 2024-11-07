e0686d4d7d886e11633cd51b3860dd82e9b3f6ba
devHksingh <hksingh.dev@gmail.com>
2024-11-07 20:40:16 +0530

    add proxy

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server:{
-  //   proxy:{
-  //     '/api':'https://simple-todo-backend-cntw.onrender.com'
-  //   }
-  // },
+  server:{
+    proxy:{
+      '/api':'https://simple-todo-backend-cntw.onrender.com'
+    }
+  },
   plugins: [react()],
 })
