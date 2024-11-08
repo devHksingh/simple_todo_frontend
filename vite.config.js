7674146b488ad162e11fe1728ac093ce933741a6
devHksingh <hksingh.dev@gmail.com>
2024-11-08 07:38:51 +0530

    testing

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://simple-todo-backend-hfsz.onrender.com',
+      '/api': 'http://localhost:5513/',
     },
   },
   plugins: [react()],
