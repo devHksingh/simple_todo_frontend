ba8663b55a0ad673fb4786470bb89c93870be82d
devHksingh <hksingh.dev@gmail.com>
2024-11-08 23:29:07 +0530

    add btn in place of anchor tag

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -138,12 +138,12 @@ function Profile() {
                     <span className="text-sm text-gray-500 opacity-50 text-inherit">Email: <span className="text-inherit">{` ${data.email}`}</span></span>
                 </div>
                 <div className="flex mt-4 md:mt-6">
-                    <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    <button className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     onClick={addTodoHandeler}
-                    >Add Todo+</a>
-                    <a href="#" className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg ms-2 focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                    >Add Todo+</button>
+                    <button className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg ms-2 focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                     onClick={logoutHandler}
-                    >Logout</a>
+                    >Logout</button>
                 </div>
             </div>
         </div>
